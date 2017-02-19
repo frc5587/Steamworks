416d770cb0f2aee5341328216f9e90f6bd1176f8
Planetaeus <galinndan.dawnfire@gmail.com>
2017-02-19 00:52:01 -0500

    Testing Values and Times

--- Getting Started/src/org/usfirst/frc/team5587/robot/commands/shooter/mortar/CANMortarPID.java
+++ Getting Started/src/org/usfirst/frc/team5587/robot/commands/shooter/mortar/CANMortarPID.java
@@ -1,5 +1,9 @@
 package org.usfirst.frc.team5587.robot.commands.shooter.mortar;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.usfirst.frc.team5587.classes.NetworkTable;
 import org.usfirst.frc.team5587.robot.Robot;
 import org.usfirst.frc.team5587.robot.subsystems.CANMortar;
 
@@ -13,6 +17,13 @@ public class CANMortarPID extends Command {
 
 	private CANMortar mortar;
 	private double targetRate;
+	private List<Double> data = new ArrayList<Double>();
+	private List<Double> times = new ArrayList<Double>();
+	private NetworkTable table;
+	private int counter;
+	private double startTime;
+	private double prevRate;
+	private boolean isRebooting;
 	
     public CANMortarPID() {
         // Use requires() here to declare subsystem dependencies
@@ -20,33 +31,70 @@ public class CANMortarPID extends Command {
     	requires( Robot.mortarCAN );
     	mortar = Robot.mortarCAN;
     	
-    	SmartDashboard.putNumber( "CAN Target Rate: ", 200.0 );
+    	table = NetworkTable.getTable( "Output data" );
+    	SmartDashboard.putNumber( "CAN Target Rate: ", -2500.0 );
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
     	mortar.speedMode();
     	//mortar.updatePID();
+    	counter = 0;
+    	prevRate = mortar.rps();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	targetRate = SmartDashboard.getNumber( "CAN Target Rate: ", -200.0 );
+    	targetRate = SmartDashboard.getNumber( "CAN Target Rate: ", -0.0 );
     	
-    	mortar.spin( targetRate );
+    	mortar.spin( targetRate );	
     	SmartDashboard.putNumber( "Encoder RPS: ", mortar.rps() );
     	SmartDashboard.putNumber( "Speed: ", mortar.rpm() );
     	System.out.println(mortar.rpm());
     	SmartDashboard.putNumber( "Motor Output", mortar.output() );
+    	
+    	times.add( this.timeSinceInitialized() );
+    	data.add( mortar.rps() );
+    
+    	if(isRebooting == true && prevRate < mortar.rps()) {
+    		double newTime = this.timeSinceInitialized() - startTime;
+    		table.putNumber("Final Time", newTime);
+    		isRebooting = false;
+    	}
+    	
+    	if(isRebooting == false && prevRate - mortar.rps() >= 1500) {
+    		isRebooting = true;
+    		startTime = this.timeSinceInitialized();
+    	}
+    	
+    	prevRate = mortar.rps();
     }
 
     // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
+    protected boolean isFinished() { 	
+    	data.add( mortar.rps() );
+    	times.add( this.timeSinceInitialized() );
+    	
+    	System.out.println( "Time: " + times.get( counter ) + ", Rate: " + data.get( counter ) );
+    	counter++;
         return false;
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	double [] sendData = new double [ data.size() ];
+    	double [] sendTime = new double [ times.size() ];
+    	
+    	for( int i = 0; i < data.size(); i++ )
+    	{
+    		sendData[ i ] = data.get( i );
+    		sendTime[ i ] = times.get( i );
+    	}
+    	
+    	table.putNumberArray( "Rate", sendData );
+    	table.putNumberArray( "Times", sendTime );
+    	
+    	mortar.stop();
     }
 
     // Called when another command which requires one or more of the same
--- Getting Started/src/org/usfirst/frc/team5587/robot/subsystems/CANMortar.java
+++ Getting Started/src/org/usfirst/frc/team5587/robot/subsystems/CANMortar.java
@@ -22,7 +22,7 @@ public class CANMortar extends Subsystem {
 	private static final int FLYWHEEL_PULSE_PER_REVOLUTION = ENC_PULSE_PER_REVOLUTION * GEAR_RATIO;
 	
 	private static double kF = .03653571429,
-						  		kP = 0.0,
+						  		kP = 0.001,
 								kI = 0.0,
 								kD = 0.0;
 								
@@ -35,7 +35,7 @@ public class CANMortar extends Subsystem {
 		flywheel = new CANTalon( RobotMap.FLYWHEEL_MOTOR_CAN_ID );
 		
 		flywheel.setFeedbackDevice( FeedbackDevice.CtreMagEncoder_Relative );
-		flywheel.configEncoderCodesPerRev( FLYWHEEL_PULSE_PER_REVOLUTION );
+		//flywheel.configEncoderCodesPerRev( FLYWHEEL_PULSE_PER_REVOLUTION );
 		
 		flywheel.configNominalOutputVoltage( +0.0f, -0.0f );
 		flywheel.configPeakOutputVoltage( +12.0f, -12.0f );
@@ -64,6 +64,7 @@ public class CANMortar extends Subsystem {
 	public void stop()
 	{
 		flywheel.set( 0 );
+		flywheel.disable();
 	}
 	
 	public double rps()
